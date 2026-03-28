af50c017554705f50c59eb975f5cb38199cd0bb3
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-28 06:56:27 +0530

    Create TPvPConfig.java

---
+++ src/main/java/com/tpvp/TPvPConfig.java
@@ -0,0 +1,6 @@
+package com.tpvp;
+
+public class TPvPConfig {
+    public static boolean heartIndicatorEnabled = true;
+    public static int indicatorMode = 0; // 0: Hearts, 1: Bar, 2: Head+Number
+}
