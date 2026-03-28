4cbfe67ca568bc058ef7aec7a7b3557f83599183
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-28 07:23:55 +0530

    Update EntityRenderStateMixin.java

--- src/main/java/com/tpvp/mixin/EntityRenderStateMixin.java
+++ src/main/java/com/tpvp/mixin/EntityRenderStateMixin.java
@@ -5,11 +5,17 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(EntityRenderState.class)
-public class EntityRenderStateMixin {
-    @Unique
-    public float tpvp$health;
-    @Unique
-    public float tpvp$maxHealth;
-    @Unique
-    public double tpvp$attackDamage;
+public class EntityRenderStateMixin implements IEntityRenderState {
+    @Unique private float health;
+    @Unique private float maxHealth;
+    @Unique private double attackDamage;
+
+    @Override public float tpvp$getHealth() { return health; }
+    @Override public void tpvp$setHealth(float h) { this.health = h; }
+
+    @Override public float tpvp$getMaxHealth() { return maxHealth; }
+    @Override public void tpvp$setMaxHealth(float mh) { this.maxHealth = mh; }
+
+    @Override public double tpvp$getAttackDamage() { return attackDamage; }
+    @Override public void tpvp$setAttackDamage(double d) { this.attackDamage = d; }
 }
