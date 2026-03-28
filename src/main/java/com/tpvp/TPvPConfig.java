39b68500f94c0d5261ca155386ca45c573ce5eb5
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-29 00:20:45 +0530

    Delete src/main/java/com/tpvp directory

--- src/main/java/com/tpvp/TPvPConfig.java
+++
@@ -1,6 +0,0 @@
-package com.tpvp;
-
-public class TPvPConfig {
-    public static boolean heartIndicatorEnabled = true;
-    public static int indicatorMode = 0; // 0: Hearts, 1: Bar, 2: Head+Number
-}
