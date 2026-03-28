3333d22dd60b5cb19768b43f43953deeda8d7611
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-29 00:39:22 +0530

    Update Mtpvp.java

--- src/main/java/com/mtpvp/Mtpvp.java
+++ src/main/java/com/mtpvp/Mtpvp.java
@@ -1,15 +1,15 @@
 package com.mtpvp;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer; // Ye change hua
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Mtpvp implements ModInitializer {
+public class Mtpvp implements ClientModInitializer { // ModInitializer ki jagah ClientModInitializer
     public static final String MOD_ID = "mtpvp";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
-    public void onInitialize() {
-        LOGGER.info("Mtpvp Client Initialized for 1.21.4!");
+    public void onInitializeClient() { // onInitialize ki jagah onInitializeClient
+        LOGGER.info("Mtpvp Client Initialized for 1.21.4 (Fixed)!");
     }
 }
