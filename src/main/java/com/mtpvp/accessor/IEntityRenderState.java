1807d0bbad6a0f14b4a5df45c19bcaf04379fabe
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-29 02:37:41 +0530

    Create IEntityRenderState.java

---
+++ src/main/java/com/mtpvp/accessor/IEntityRenderState.java
@@ -0,0 +1,10 @@
+package com.tpvp.accessor;
+
+public interface IEntityRenderState {
+    float tpvp$getHealth();
+    void tpvp$setHealth(float health);
+    float tpvp$getMaxHealth();
+    void tpvp$setMaxHealth(float maxHealth);
+    double tpvp$getAttackDamage();
+    void tpvp$setAttackDamage(double damage);
+}
