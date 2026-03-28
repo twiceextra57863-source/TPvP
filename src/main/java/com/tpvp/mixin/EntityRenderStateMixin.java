bccfe55b857ceb0f1b728664490b58ef82874e80
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-28 07:35:48 +0530

    Update EntityRenderStateMixin.java

--- src/main/java/com/tpvp/mixin/EntityRenderStateMixin.java
+++ src/main/java/com/tpvp/mixin/EntityRenderStateMixin.java
@@ -1,5 +1,6 @@
 package com.tpvp.mixin;
 
+import com.tpvp.accessor.IEntityRenderState; // Import naya package
 import net.minecraft.client.render.entity.state.EntityRenderState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -12,10 +13,8 @@ public class EntityRenderStateMixin implements IEntityRenderState {
 
     @Override public float tpvp$getHealth() { return health; }
     @Override public void tpvp$setHealth(float h) { this.health = h; }
-
     @Override public float tpvp$getMaxHealth() { return maxHealth; }
     @Override public void tpvp$setMaxHealth(float mh) { this.maxHealth = mh; }
-
     @Override public double tpvp$getAttackDamage() { return attackDamage; }
     @Override public void tpvp$setAttackDamage(double d) { this.attackDamage = d; }
 }
