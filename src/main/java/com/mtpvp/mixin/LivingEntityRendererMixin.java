f6ff776e54ada94252f9da01e0a6a499862e17d8
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-29 02:38:34 +0530

    Create LivingEntityRendererMixin.java

---
+++ src/main/java/com/mtpvp/mixin/LivingEntityRendererMixin.java
@@ -0,0 +1,30 @@
+package com.tpvp.mixin;
+
+import com.tpvp.accessor.IEntityRenderState;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.client.render.entity.state.LivingEntityRenderState;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttributes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LivingEntityRenderer.class)
+public class LivingEntityRendererMixin<T extends LivingEntity, S extends LivingEntityRenderState> {
+    
+    @Inject(method = "updateRenderState", at = @At("TAIL"))
+    private void onUpdateState(T entity, S state, float tickDelta, CallbackInfo ci) {
+        if (state instanceof IEntityRenderState access) {
+            access.tpvp$setHealth(entity.getHealth());
+            access.tpvp$setMaxHealth(entity.getMaxHealth());
+            
+            var player = MinecraftClient.getInstance().player;
+            if (player != null) {
+                // Gets damage based on currently held weapon (Diamond sword/Axe etc)
+                access.tpvp$setAttackDamage(player.getAttributeValue(EntityAttributes.ATTACK_DAMAGE));
+            }
+        }
+    }
+}
