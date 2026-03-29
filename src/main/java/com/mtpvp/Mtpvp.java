edfd74f358dba2bfa39cdba5fc6b39a0a535428a
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-29 20:05:06 +0530

    Update Mtpvp.java

--- src/main/java/com/mtpvp/Mtpvp.java
+++ src/main/java/com/mtpvp/Mtpvp.java
@@ -4,32 +4,33 @@ import com.mtpvp.gui.MtpvpDashboard;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
-public class Mtpvp implements ClientModInitializer {
+public class MtpvpClient implements ClientModInitializer {
     public static KeyBinding targetKey;
 
     @Override
     public void onInitializeClient() {
+        // 'R' Keybind for Target
         targetKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.mtpvp.target", 
-                InputUtil.Type.KEYSYM, 
-                GLFW.GLFW_KEY_R, 
-                "category.mtpvp"
+                "key.mtpvp.target",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_R,
+                "MTPVP Client"
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (targetKey.wasPressed()) {
-                // Quick Target Logic: Look at a player and press R
                 if (client.targetedEntity instanceof PlayerEntity target) {
-                    MtpvpDashboard.targetPlayerName = target.getName().getString();
-                    client.player.sendMessage(net.minecraft.text.Text.of("§c§l[MTPVP] §fTarget Set: §e" + MtpvpDashboard.targetPlayerName), true);
+                    MtpvpDashboard.targetPlayerName = target.getGameProfile().getName();
+                    client.player.sendMessage(Text.literal("§c§l[MTPVP] §fTarget Locked: §e" + MtpvpDashboard.targetPlayerName), true);
                 } else {
-                    MtpvpDashboard.targetPlayerName = ""; // Clear target if looking at air
+                    MtpvpDashboard.targetPlayerName = "";
+                    client.player.sendMessage(Text.literal("§7§l[MTPVP] §fTarget Cleared"), true);
                 }
             }
         });
