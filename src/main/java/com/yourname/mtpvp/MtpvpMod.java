dc0e4415de7fc79b8e55606b07d8632535eb8a79
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-29 22:05:54 +0530

    Update MtpvpMod.java

--- src/main/java/com/yourname/mtpvp/MtpvpMod.java
+++ src/main/java/com/yourname/mtpvp/MtpvpMod.java
@@ -1,13 +1,12 @@
 package com.mtpvp;
 
 import com.mtpvp.config.MtpvpConfig;
-import com.mtpvp.renderer.HealthRenderer;
 import net.fabricmc.api.ClientModInitializer;
 
 public class MtpvpMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        MtpvpConfig.load(); // Load settings first
-        HealthRenderer.init();
+        MtpvpConfig.load();
+        System.out.println("Mtpvp Client with Mixin Rendering Loaded!");
     }
 }
