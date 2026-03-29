73bb79a6f24379e3bd777e1c77c62f26363af4f4
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-29 21:47:10 +0530

    Update MtpvpMod.java

--- src/main/java/com/yourname/mtpvp/MtpvpMod.java
+++ src/main/java/com/yourname/mtpvp/MtpvpMod.java
@@ -6,6 +6,8 @@ import net.fabricmc.api.ClientModInitializer;
 public class MtpvpMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        // Indicator ko initialize karna
         HealthRenderer.init();
+        System.out.println("Mtpvp Client Mod Initialized!");
     }
 }
