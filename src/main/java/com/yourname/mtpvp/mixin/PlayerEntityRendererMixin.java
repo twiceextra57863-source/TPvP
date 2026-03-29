d91fd5d19168cefa5c05baf0c18a08ce4817706f
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-29 22:58:22 +0530

    Delete src/main/java/com/yourname/mtpvp directory

--- src/main/java/com/yourname/mtpvp/mixin/PlayerEntityRendererMixin.java
+++
@@ -1,9 +0,0 @@
-package com.mtpvp.mixin;
-
-import net.minecraft.client.render.entity.PlayerEntityRenderer;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(PlayerEntityRenderer.class)
-public abstract class PlayerEntityRendererMixin {
-    // Ye LivingEntityRendererMixin se inherit ho jayega
-}
