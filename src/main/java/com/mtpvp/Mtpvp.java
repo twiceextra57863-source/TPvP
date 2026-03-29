a26a34ab0c27fc892ff1426968d7c500bb0c66e3
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-29 14:29:56 +0530

    Update Mtpvp.java

--- src/main/java/com/mtpvp/Mtpvp.java
+++ src/main/java/com/mtpvp/Mtpvp.java
@@ -1,15 +1,37 @@
 package com.mtpvp;
 
-import net.fabricmc.api.ClientModInitializer; // Ye change hua
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.mtpvp.gui.MtpvpDashboard;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.player.PlayerEntity;
+import org.lwjgl.glfw.GLFW;
 
-public class Mtpvp implements ClientModInitializer { // ModInitializer ki jagah ClientModInitializer
-    public static final String MOD_ID = "mtpvp";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+public class Mtpvp implements ClientModInitializer {
+    public static KeyBinding targetKey;
 
     @Override
-    public void onInitializeClient() { // onInitialize ki jagah onInitializeClient
-        LOGGER.info("Mtpvp Client Initialized for 1.21.4 (Fixed)!");
+    public void onInitializeClient() {
+        targetKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.mtpvp.target", 
+                InputUtil.Type.KEYSYM, 
+                GLFW.GLFW_KEY_R, 
+                "category.mtpvp"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (targetKey.wasPressed()) {
+                // Quick Target Logic: Look at a player and press R
+                if (client.targetedEntity instanceof PlayerEntity target) {
+                    MtpvpDashboard.targetPlayerName = target.getName().getString();
+                    client.player.sendMessage(net.minecraft.text.Text.of("§c§l[MTPVP] §fTarget Set: §e" + MtpvpDashboard.targetPlayerName), true);
+                } else {
+                    MtpvpDashboard.targetPlayerName = ""; // Clear target if looking at air
+                }
+            }
+        });
     }
 }
