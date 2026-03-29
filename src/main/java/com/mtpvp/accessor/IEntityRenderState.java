8d3932fe52fc714dbede5b40bd728a27f7a51638
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-29 20:54:40 +0530

    Delete src/main/java/com/mtpvp directory

--- src/main/java/com/mtpvp/accessor/IEntityRenderState.java
+++
@@ -1,8 +0,0 @@
-package com.mtpvp.accessor;
-
-public interface IEntityRenderState {
-    float mtpvp$getHealth();
-    float mtpvp$getMaxHealth();
-    void mtpvp$setHealth(float health);
-    void mtpvp$setMaxHealth(float maxHealth);
-}
