488942e7c8d4eee110268c39edf90afe13e8fcee
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-31 00:37:58 +0530

    Update InGameMenuMixin.java

--- src/main/java/com/yourname/mtpvp/mixin/InGameMenuMixin.java
+++ src/main/java/com/yourname/mtpvp/mixin/InGameMenuMixin.java
@@ -2,7 +2,7 @@ package com.yourname.mtpvp.mixin;
 
 import com.yourname.mtpvp.client.gui.MtpvpDashboardScreen;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,18 +10,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
-public class TitleScreenMixin extends Screen {
+@Mixin(GameMenuScreen.class)
+public class InGameMenuMixin extends Screen {
     
-    protected TitleScreenMixin(Text title) {
+    protected InGameMenuMixin(Text title) {
         super(title);
     }
     
     @Inject(method = "init", at = @At("TAIL"))
     private void onInit(CallbackInfo ci) {
-        // Position button away from quit button (above or below)
-        // Quit button is usually at y = height / 4 + 96 + 48 + 24
-        // Let's put our button above it
+        // Position button away from quit button
+        // Put it above the quit button
         this.addDrawableChild(ButtonWidget.builder(Text.literal("⚔️ MTPVP"), button -> {
             if (client != null) {
                 client.setScreen(new MtpvpDashboardScreen(this));
