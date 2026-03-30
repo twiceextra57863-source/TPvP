da4971935c48136215220faef3433c0c498cca93
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-30 20:09:49 +0530

    Update InGameMenuMixin.java

--- src/main/java/com/yourname/mtpvp/mixin/InGameMenuMixin.java
+++ src/main/java/com/yourname/mtpvp/mixin/InGameMenuMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameMenuScreen.class)
-public abstract class InGameMenuMixin extends Screen {
+public class InGameMenuMixin extends Screen {
     
     protected InGameMenuMixin(Text title) {
         super(title);
