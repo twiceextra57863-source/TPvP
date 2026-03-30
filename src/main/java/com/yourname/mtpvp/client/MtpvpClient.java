f57d5c77b14bad979bb1f2b539c72c4c8b1d07dc
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-30 23:52:54 +0530

    Update MtpvpClient.java

--- src/main/java/com/yourname/mtpvp/client/MtpvpClient.java
+++ src/main/java/com/yourname/mtpvp/client/MtpvpClient.java
@@ -1,14 +1,12 @@
 package com.yourname.mtpvp.client;
 
 import com.yourname.mtpvp.client.event.HeartIndicatorHandler;
-import com.yourname.mtpvp.client.event.ScreenButtonHandler;
 import net.fabricmc.api.ClientModInitializer;
 
 public class MtpvpClient implements ClientModInitializer {
     
     @Override
     public void onInitializeClient() {
-        ScreenButtonHandler.register();
         HeartIndicatorHandler.register();
         System.out.println("MTPVP Client initialized!");
     }
