4b3ac05e49dd2dff44285e25ca0f73f1c1e1c618
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-30 18:57:14 +0530

    Create MtpvpClient.java

---
+++ src/main/java/com/yourname/mtpvp/client/MtpvpClient.java
@@ -0,0 +1,14 @@
+package com.yourname.mtpvp.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class MtpvpClient implements ClientModInitializer {
+    
+    @Override
+    public void onInitializeClient() {
+        // Client-side initialization
+    }
+}
