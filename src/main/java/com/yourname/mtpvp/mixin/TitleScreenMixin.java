7e350755507ee91cb2d97aafafefc1ea9e6346a9
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-30 18:58:50 +0530

    Create TitleScreenMixin.java

---
+++ src/main/java/com/yourname/mtpvp/mixin/TitleScreenMixin.java
@@ -0,0 +1,29 @@
+package com.yourname.mtpvp.mixin;
+
+import com.yourname.mtpvp.client.gui.MtpvpDashboardScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public abstract class TitleScreenMixin extends Screen {
+    
+    protected TitleScreenMixin(Text title) {
+        super(title);
+    }
+    
+    @Inject(method = "init", at = @At("TAIL"))
+    private void onInit(CallbackInfo ci) {
+        // Add Mtpvp button to title screen
+        this.addDrawableChild(ButtonWidget.builder(Text.literal("Mtpvp"), button -> {
+            if (client != null) {
+                client.setScreen(new MtpvpDashboardScreen(this));
+            }
+        }).dimensions(this.width / 2 - 100, this.height / 4 + 96 + 48, 200, 20).build());
+    }
+}
