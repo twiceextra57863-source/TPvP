c3d0ff3e536dece8e1a49f39a14d7c70d8b02560
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-30 18:21:47 +0530

    Create HeartIndicatorMod.java

---
+++ src/main/java/com/heartindicator/HeartIndicatorMod.java
@@ -0,0 +1,16 @@
+package com.heartindicator;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HeartIndicatorMod implements ModInitializer {
+
+    public static final String MOD_ID = "heartindicator";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("[HeartIndicator] Mod loaded successfully!");
+    }
+}
