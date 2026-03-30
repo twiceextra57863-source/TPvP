6341ea3a53d1d0d0d0a783e2e04ea1000c9c2ec4
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-30 18:59:08 +0530

    Create InGameMenuMixin.java

---
+++ src/main/java/com/yourname/mtpvp/mixin/InGameMenuMixin.java
@@ -0,0 +1,29 @@
+package com.yourname.mtpvp.mixin;
+
+import com.yourname.mtpvp.client.gui.MtpvpDashboardScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.InGameMenuScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameMenuScreen.class)
+public abstract class InGameMenuMixin extends Screen {
+    
+    protected InGameMenuMixin(Text title) {
+        super(title);
+    }
+    
+    @Inject(method = "init", at = @At("TAIL"))
+    private void onInit(CallbackInfo ci) {
+        // Add Mtpvp button to escape menu
+        this.addDrawableChild(ButtonWidget.builder(Text.literal("Mtpvp"), button -> {
+            if (client != null) {
+                client.setScreen(new MtpvpDashboardScreen(this));
+            }
+        }).dimensions(this.width / 2 - 100, this.height / 4 + 96 + 48, 200, 20).build());
+    }
+}
