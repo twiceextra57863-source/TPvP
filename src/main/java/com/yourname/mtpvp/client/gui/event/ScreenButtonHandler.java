f0a65a963fb03aeadc1011ed2e1f673e5b6e2f3e
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-30 20:44:31 +0530

    Update ScreenButtonHandler.java

--- src/main/java/com/yourname/mtpvp/client/gui/event/ScreenButtonHandler.java
+++ src/main/java/com/yourname/mtpvp/client/gui/event/ScreenButtonHandler.java
@@ -2,6 +2,7 @@ package com.yourname.mtpvp.client.event;
 
 import com.yourname.mtpvp.client.gui.MtpvpDashboardScreen;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenMouseEvents;
 import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
@@ -21,7 +22,8 @@ public class ScreenButtonHandler {
                     button -> client.setScreen(new MtpvpDashboardScreen(screen))
                 ).dimensions(centerX - 100, buttonY, 200, 20).build();
                 
-                screen.addDrawableChild(mtpvpButton);
+                // Use addDrawable instead of addDrawableChild
+                screen.addDrawable(mtpvpButton);
             }
             
             // Add button to Escape Menu
@@ -34,7 +36,8 @@ public class ScreenButtonHandler {
                     button -> client.setScreen(new MtpvpDashboardScreen(screen))
                 ).dimensions(centerX - 100, buttonY, 200, 20).build();
                 
-                screen.addDrawableChild(mtpvpButton);
+                // Use addDrawable instead of addDrawableChild
+                screen.addDrawable(mtpvpButton);
             }
         });
     }
