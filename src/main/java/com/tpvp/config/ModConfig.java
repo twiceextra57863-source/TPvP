602e5cadd86e590dc7d6d1e098c4ba7bc54c03bd
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-01 00:09:40 +0530

    Update ModConfig.java

--- src/main/java/com/tpvp/config/ModConfig.java
+++ src/main/java/com/tpvp/config/ModConfig.java
@@ -1,12 +1,14 @@
 package com.tpvp.config;
 
 public class ModConfig {
-    // Combat Settings
     public static boolean indicatorEnabled = true;
     public static int indicatorStyle = 1; 
 
-    // Radar / Nearby Settings (New)
     public static boolean nearbyEnabled = true;
+    // Naye variables Custom HUD ke liye
+    public static int hudX = 10;
+    public static int hudY = 30;
+    public static float hudScale = 1.0f;
 
     public static void load() { }
     public static void save() { }
