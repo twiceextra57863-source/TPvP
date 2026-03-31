faef77a34ea6c763791e819ab82eda6f011fbcf4
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-31 17:16:19 +0530

    Update TPvPClient.java

--- src/main/java/com/tpvp/TPvPClient.java
+++ src/main/java/com/tpvp/TPvPClient.java
@@ -2,15 +2,19 @@ package com.tpvp;
 
 import com.tpvp.config.ModConfig;
 import com.tpvp.hud.Indicator3D;
+import com.tpvp.hud.NearbyPlayersHud;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 public class TPvPClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // Config load
         ModConfig.load();
         
-        // Naya 3D Name Tag Indicator start karna
+        // 3D Hit Indicator register
         Indicator3D.register();
+
+        // 2D Nearby Players Radar HUD register
+        HudRenderCallback.EVENT.register(new NearbyPlayersHud());
     }
 }
