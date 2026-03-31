2f239c2e2437319bdd6fe34e0c0bed646f0d53d6
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-31 17:14:44 +0530

    Update ModConfig.java

--- src/main/java/com/tpvp/config/ModConfig.java
+++ src/main/java/com/tpvp/config/ModConfig.java
@@ -1,15 +1,13 @@
 package com.tpvp.config;
 
 public class ModConfig {
-    // Ye variables hamara dashboard control karega
+    // Combat Settings
     public static boolean indicatorEnabled = true;
-    public static int indicatorStyle = 0; // 0 = Heart, 1 = Bar, 2 = Head + Number
+    public static int indicatorStyle = 1; 
 
-    // Future me ise JSON file me save karne ka logic add kar sakte ho
-    public static void load() {
-        // Dummy load method
-    }
-    public static void save() {
-        // Dummy save method
-    }
+    // Radar / Nearby Settings (New)
+    public static boolean nearbyEnabled = true;
+
+    public static void load() { }
+    public static void save() { }
 }
