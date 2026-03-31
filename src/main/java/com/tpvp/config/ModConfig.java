734496aa7a0ae903fb61ff0aff25a468f8bbae1a
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-31 10:02:43 +0530

    Create ModConfig.java

---
+++ src/main/java/com/tpvp/config/ModConfig.java
@@ -0,0 +1,15 @@
+package com.tpvp.config;
+
+public class ModConfig {
+    // Ye variables hamara dashboard control karega
+    public static boolean indicatorEnabled = true;
+    public static int indicatorStyle = 0; // 0 = Heart, 1 = Bar, 2 = Head + Number
+
+    // Future me ise JSON file me save karne ka logic add kar sakte ho
+    public static void load() {
+        // Dummy load method
+    }
+    public static void save() {
+        // Dummy save method
+    }
+}
