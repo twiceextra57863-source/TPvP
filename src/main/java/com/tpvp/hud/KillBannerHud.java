be36731a9aa3b4c6715a6ad383caa1b085f33996
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-13 07:07:55 +0530

    Update KillBannerHud.java

--- src/main/java/com/tpvp/hud/KillBannerHud.java
+++ src/main/java/com/tpvp/hud/KillBannerHud.java
@@ -2,10 +2,6 @@ package com.tpvp.hud;
 
 import com.tpvp.config.ModConfig;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-package com.tpvp.hud;
-
-import com.tpvp.config.ModConfig;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.RenderLayer;
