558959f138a27552776c90ef93a914439ed80bb3
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-16 14:59:20 +0530

    Update TPvPClient.java

--- src/main/java/com/tpvp/TPvPClient.java
+++ src/main/java/com/tpvp/TPvPClient.java
@@ -5,20 +5,31 @@ import com.tpvp.hud.Indicator3D;
 import com.tpvp.hud.NearbyPlayersHud;
 import com.tpvp.hud.ArmorHud;
 import com.tpvp.hud.KillBannerHud;
-import com.tpvp.hud.PotionHud; // NAYA IMPORT!
+import com.tpvp.hud.PotionHud;
+import com.tpvp.hud.PvPStatsManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 public class TPvPClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        // --- LOAD SAVED DATA WHEN GAME STARTS ---
         ModConfig.load();
+        PvPStatsManager.load();
         
+        // 3D Rendering Registers
         Indicator3D.register();
         
+        // 2D HUD Registers
         HudRenderCallback.EVENT.register(new NearbyPlayersHud());
         HudRenderCallback.EVENT.register(new ArmorHud());
         HudRenderCallback.EVENT.register(new KillBannerHud());
-        HudRenderCallback.EVENT.register(new PotionHud()); // Potion HUD Zinda ho gaya!
+        HudRenderCallback.EVENT.register(new PotionHud()); 
+
+        // --- SHUTDOWN HOOK: ALWAYS SAVE BEFORE GAME CLOSES ---
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            ModConfig.save();
+            PvPStatsManager.save();
+        }));
     }
 }
